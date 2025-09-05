f9496b3120d99754302a3a29a9380e3d0d27e02a
Chethan Reddy <nagachethanreddymandati@gmail.com>
2025-09-05 13:03:39 -0500

    Update create_tables.py

--- ShopMart/backend/create_tables.py
+++ ShopMart/backend/create_tables.py
@@ -1,10 +1,9 @@
-# create_tables.py
-from database import engine
-import models
+# backend/create_tables.py
+from backend.database import engine   # 🔹 absolute import
+from backend import models            # 🔹 absolute import
 
 print("🔄 Creating tables...")
 
-# This will create tables if they don't exist already
 models.Base.metadata.create_all(bind=engine)
 
 print("✅ Tables created successfully!")
